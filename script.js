70cc64ff9771ea64fde8ed18c20c6d19ca337ce5
Ayush Sharma <117670536+ayusharma-ctrl@users.noreply.github.com>
2023-01-14 18:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var orderedList = document.getElementById("orderedList")
 var prevBtn = document.getElementById("load_prev")
 var nextBtn = document.getElementById("load_next")
 
-var pageNumberHere = 1
+var i = 1
 
-var apiLink = `https://api.github.com/repositories/1296269/issues?page= + ${pageNumberHere}  + &per_page=5`
+var apiLink = `https://api.github.com/repositories/1296269/issues?page= + ${i}  + &per_page=5`
 
 async function fetching() {
     try {
@@ -21,22 +21,25 @@ async function fetching() {
 
 document.addEventListener('DOMContentLoaded', fetching)
 
-
+var listItem = ''
 function insert(dataName) {
     dataName.map((e) => {
-        span.textContent = pageNumberHere
-        var listItem = document.createElement("li")
-        listItem.textContent = e.title
-        orderedList.append(listItem)
+       span.textContent = i
+        // var listItem = document.createElement("li")
+        // listItem.textContent = e.title
+        // orderedList.append(listItem)
+        listItem += `<li>${e.title}</li>`
     })
+    orderedList.innerHTML = listItem
     console.log(dataName)
 }
 
 
 function nextPage() {
-    orderedList.textContent = ""
-    pageNumberHere++
-    apiLink = `https://api.github.com/repositories/1296269/issues?page= + ${pageNumberHere}  + &per_page=5`
+    // orderedList.textContent = ''
+    listItem = ''
+    i++
+    apiLink = `https://api.github.com/repositories/1296269/issues?page= + ${i}  + &per_page=5`
     fetching()
 }
 
@@ -44,10 +47,11 @@ nextBtn.addEventListener('click', nextPage)
 
 
 function prevPage() {
-    if (pageNumberHere > 1) {
-        orderedList.textContent = ""
-        pageNumberHere--
-        apiLink = `https://api.github.com/repositories/1296269/issues?page= + ${pageNumberHere}  + &per_page=5`
+    if (i > 1) {
+        // orderedList.textContent = ""
+        listItem = ''
+        i--
+        apiLink = `https://api.github.com/repositories/1296269/issues?page= + ${i}  + &per_page=5`
         fetching()
     }
 }
